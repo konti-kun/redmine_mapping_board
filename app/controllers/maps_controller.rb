097069252336ac9999f9d79467e6497ea81c821f
konti-kun <yasu211211yasu@gmail.com>
2019-01-16 23:40:49 +0900

    draw rect by position objects

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,6 +4,12 @@ class MapsController < ApplicationController
   before_filter :find_project, :authorize, :only => :index
 
   def index
+    @items = Position.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @items }
+    end
   end
 
   private
--- assets/javascripts/drawmap.js
+++ assets/javascripts/drawmap.js
@@ -1,1 +1,10 @@
-d3.select(".svg_panel").append("svg");
+var svg = d3.select(".svg_panel").append("svg").attr("width",1000).attr("height",500);
+svg.selectAll("g").data(datas).enter().append("g")
+.attr("transform",function(d){return "translate(" + d.x + "," + d.y + ")"})
+.each(function(d){
+  d3.select(this).append("rect")
+  .attr("x",0).attr("y",0)
+  .attr("height",100).attr("width",200)
+  .attr("stroke","black").attr("fill", "#F9F5E8"); 
+});
+
