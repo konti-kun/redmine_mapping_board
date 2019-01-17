6d172b7912445acc12af9b622eb9fa2dac05da0e
konti-kun <yasu211211yasu@gmail.com>
2019-01-18 02:27:48 +0900

    upload file by POST via d3.js

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -12,9 +12,23 @@ class MapsController < ApplicationController
     end
   end
 
+  def uploadfile
+    uploaded_file = fileupload_param.tempfile
+    output_path = Rails.root.join('public', fileupload_param.original_filename)
+    puts output_path
+    File.open(output_path, 'w+b') do |fp|
+      fp.write  uploaded_file.read
+    end
+    render json: {status: 200,data: @items}
+  end
+
   private
 
   def find_project
     @project = Project.find(params[:project_id])
+  end 
+
+  def fileupload_param
+	  params.require(:uploadfile)
   end
 end
