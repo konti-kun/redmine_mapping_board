a753b13beeac8851cafd3d6083720a82ff4c8510
konti-kun <yasu211211yasu@gmail.com>
2019-01-20 00:15:02 +0900

    add Rect Class and write test code

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,2 +1,10 @@
 class Position < ActiveRecord::Base
+  def update_position(x, y, image, rect)
+    trans_x = rect.width.to_f / image.width
+    trans_y = rect.height.to_f / image.height
+
+    @x = rect.x + x*trans_x
+    @y = rect.y + y*trans_y
+  end
+
 end
