7fb2c0c594dd8be9aa11850718698fe7e8e46839
y-kondo <y-kondo@eu.jp.nec.com>
2019-01-26 23:17:14 +0900

    update d3.js source for draw notes

--- assets/javascripts/drawmap.js
+++ assets/javascripts/drawmap.js
@@ -1,10 +1,25 @@
-var svg = d3.select(".svg_panel").append("svg").attr("width",1000).attr("height",500);
-svg.selectAll("g").data(datas).enter().append("g")
-.attr("transform",function(d){return "translate(" + d.x + "," + d.y + ")"})
-.each(function(d){
-  d3.select(this).append("rect")
-  .attr("x",0).attr("y",0)
-  .attr("height",100).attr("width",200)
-  .attr("stroke","black").attr("fill", "#F9F5E8"); 
-});
+function update_notes(data,svg){
+  var note = svg.selectAll("g").data(data)
+  
+  note.exit().remove();
+  
+  note
+  .enter().append("g")
+  .attr("transform",function(d){return "translate(0,0)"})
+  .each(function(d){
+    var note_width = 150;
+    var note_height= 120;
+    var div_style = "font-size:10px;word-wrap:break-word;width:" + (note_width-10) + "px;height:" + (note_height-10)+"px;";
+    var note_node = d3.select(this);
+    note_node.append("rect").attr("width", note_width).attr("height", note_height).attr("stroke","black").attr("fill","white")
+    .attr("filter","url(#drop-shadow)");
+    note_node.append("rect").attr("width", note_width).attr("height", note_height).attr("stroke","black").attr("fill","white");
+    svg.append("foreignObject").attr("x",5).attr("y",5).attr("width",note_width-10).attr("height",note_height-10)
+    .append("xhtml:div").attr("style",div_style);
+  })
+  .merge(note)
+  .transition()
+  .duration(750)
+  .attr("transform",function(d){return "translate(" + d.x + "," + d.y + ")"})
+}
 
