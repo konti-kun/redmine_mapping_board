e914a87ffe2e57c4fe69d60d097670b391e441d1
konti-kun <konti-kun@users.noreply.github.com>
2019-04-07 23:28:54 +0900

    first write system-spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,13 @@
+require 'selenium/webdriver'
 require File.expand_path(File.dirname(__FILE__) + '/../../../spec/spec_helper')
 RSpec.configure do |config|
-  config.mock_with :mocha
+  config.before(:each) do |example|
+    if example.metadata[:type] == :system
+      if example.metadata[:js]
+        driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+      else
+        driven_by :rack_test
+      end
+    end
+  end
 end
