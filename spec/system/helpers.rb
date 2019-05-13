082e0b4bd6437f8eaf2c000e4ec809e125064747
konti-kun <konti-kun@users.noreply.github.com>
2019-05-13 09:29:15 +0900

    fix login logic in spec helper

--- spec/system/helpers.rb
+++ spec/system/helpers.rb
@@ -1,11 +1,12 @@
 module Helpers
   def log_user(login, password)
     visit '/my/page'
-    assert_equal '/login', current_path
-    within('#login-form form') do
-      fill_in 'username', with: login
-      fill_in 'password', with: password
-      find('input[name=login]').click
+    if  '/login' == current_path
+      within('#login-form form') do
+        fill_in 'username', with: login
+        fill_in 'password', with: password
+        find('input[name=login]').click
+      end
     end
     assert_equal '/my/page', current_path
   end
